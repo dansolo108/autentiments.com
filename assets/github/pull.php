42ad1d778981d95e2e889920154a819ae4c4d9a1
skril <SZhukovWork@gmail.com>
2022-07-14 21:59:40 +0500

    github webHook

--- assets/github/pull.php
+++ assets/github/pull.php
@@ -6,7 +6,4 @@ $modx->setLogTarget('ECHO');
 $modx->setLogLevel(MODX_LOG_LEVEL_INFO);
 $output = shell_exec('git -C "'.MODX_BASE_PATH.'" pull');
 $modx->cacheManager->refresh();
-$modx->log(3,print_r('git -C "'.MODX_BASE_PATH.'" pull',1));
-$modx->log(3,print_r($output,1));
-echo 'test';
-die();
+die($output);
