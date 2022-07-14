f2df2f44033bd7f96fa4d7589e024dd17fa08462
skril <SZhukovWork@gmail.com>
2022-07-14 21:46:26 +0500

    github webHook

---
+++ assets/github/pull.php
@@ -0,0 +1,11 @@
+<?php
+define('MODX_API_MODE', true);
+require_once dirname(dirname(__DIR__)) . '/index.php';
+/** @var $modx gitModx */
+$modx->setLogTarget('ECHO');
+$modx->setLogLevel(MODX_LOG_LEVEL_INFO);
+$output = shell_exec('git -C "'.MODX_BASE_PATH.'" pull');
+$modx->cacheManager->refresh();
+$modx->log(3,print_r('git -C "'.MODX_BASE_PATH.'" pull',1));
+$modx->log(3,print_r($output,1));
+die();
--- sitecore/components/gitmodx/elements/plugins/gitWebhook.php
+++ sitecore/components/gitmodx/elements/plugins/gitWebhook.php
@@ -2,21 +2,3 @@
 /** @var $modx gitModx */
 if($modx->event->name != 'OnHandleRequest') return;
 
-$alias = $modx->context->getOption('request_param_alias', 'q');
-if (!isset($_REQUEST[$alias])) {return;}
-// А если есть - работаем дальше
-$request = $_REQUEST[$alias];
-$tmp = explode('/', $request);
-
-if ($tmp[0] == 'github') {
-    $output = exec('git -C "'.MODX_BASE_PATH.'" pull');
-    $modx->log(1,print_r('git -C "'.MODX_BASE_PATH.'" pull',1));
-    $modx->log(1,print_r($output,1));
-    $modx->log(1,print_r('test',1));
-
-    $modx->cacheManager->refresh();
-
-}
-else{
-    return;
-}
