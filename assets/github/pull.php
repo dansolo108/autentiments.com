9b5526a7c13691e8dfcb8762ca7ce793561d2b4a
skril <SZhukovWork@gmail.com>
2022-07-14 23:36:14 +0500

    github webHook

--- assets/github/pull.php
+++ assets/github/pull.php
@@ -1,6 +1,5 @@
-<?php
+<?php `git pull`;
 define('MODX_API_MODE', true);
 require_once dirname(dirname(__DIR__)) . '/index.php';
 /** @var $modx gitModx */
 $modx->cacheManager->refresh();
-`git pull`;
