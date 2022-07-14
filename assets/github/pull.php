34f0de79dbb782a0ec350c394df41cb1ef6e13cb
skril <SZhukovWork@gmail.com>
2022-07-14 21:47:59 +0500

    github webHook

--- assets/github/pull.php
+++ assets/github/pull.php
@@ -8,4 +8,5 @@ $output = shell_exec('git -C "'.MODX_BASE_PATH.'" pull');
 $modx->cacheManager->refresh();
 $modx->log(3,print_r('git -C "'.MODX_BASE_PATH.'" pull',1));
 $modx->log(3,print_r($output,1));
+echo 'test';
 die();
