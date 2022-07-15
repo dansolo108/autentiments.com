d83b60a771a173362fd232ca3afb72649142c4a2
skril <SZhukovWork@gmail.com>
2022-07-15 21:45:00 +0500

    git готов

--- assets/github/pull.php
+++ assets/github/pull.php
@@ -4,5 +4,4 @@ require_once dirname(dirname(__DIR__)) . '/index.php';
 /** @var $modx gitModx */
 $modx->cacheManager->refresh();
 $modx->log(1,print_r(file_get_contents('php://input'),1));
-$modx->log(1,print_r($_GET,1));
 
