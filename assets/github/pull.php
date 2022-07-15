5014f978bf77b3614aac35c5a24cd4ede0cfa696
skril <SZhukovWork@gmail.com>
2022-07-15 21:51:11 +0500

    git готов

--- assets/github/pull.php
+++ assets/github/pull.php
@@ -3,7 +3,7 @@ define('MODX_API_MODE', true);
 require_once dirname(dirname(__DIR__)) . '/index.php';
 /** @var $modx gitModx */
 $input = json_decode(file_get_contents('php://input'),1);
-if(strpos($input['ref'],$modx->getOption('github_branch_on_push',[],'main'))){
+if(strpos($input['ref'],$modx->getOption('github_branch_on_push',[],'main')) !== false){
     `git pull`;
     $modx->cacheManager->refresh();
 }
