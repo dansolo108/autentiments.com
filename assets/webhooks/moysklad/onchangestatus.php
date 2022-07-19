a5af29f7006b9ba91e139c16992496a2f162e878
skril <SZhukovWork@gmail.com>
2022-07-19 16:01:25 +0500

    webhook plugin to file

--- assets/webhooks/moysklad/onchangestatus.php
+++ assets/webhooks/moysklad/onchangestatus.php
@@ -2,7 +2,7 @@
 define('MODX_API_MODE', true);
 require_once dirname(__DIR__, 3) . '/index.php';
 /** @var $modx gitModx */
-$modx->setLogTarget('ECHO');
+$modx->setLogTarget('HTML');
 $modx->setLogLevel(MODX_LOG_LEVEL_INFO);
 $input = json_decode(file_get_contents('php://input'),1);
 $myCurl = curl_init();
