894886cc442ca0fc360980554a8b00afbc4e6d3d
skril <SZhukovWork@gmail.com>
2022-09-27 21:19:40 +0500

    fix

--- sitecore/components/autentiments/model/autentiments/modification.class.php
+++ sitecore/components/autentiments/model/autentiments/modification.class.php
@@ -6,22 +6,10 @@ class Modification extends xPDOSimpleObject {
     }
 
     function getDetail($name){
-        $type = $this->xpdo->getObject('DetailType',['name'=>$name]);
-        if(empty($type)){
-            return null;
-        }
-        $details = $this->getMany('Details',['type_id'=>$type->get('id')]);
-        if(count($details) !== 0) {
-            if (count($details) > 1) {
-                $this->xpdo->log(MODX_LOG_LEVEL_ERROR, 'detail find error count details > 1 modification id:' . $this->get('id') . ', detail type id:' . $type->get('id') . 'name:'.$name);
-                foreach($details as $detail){
-                    $this->xpdo->log(MODX_LOG_LEVEL_ERROR, 'detail'. var_export($detail->toArray(),1));
-
-                }
-            }
-            return array_pop($details);
-        }
-        return null;
+        $q = $this->xpdo->newQuery('ModificationDetail');
+        $q->where(['ModificationDetail.modification_id'=>$this->get('id'), 'DetailType'=>$name]);
+        $q->leftJoin('DetailType','DetailType',['ModificationDetail.type_id = DetailType.id']);
+        return $this->xpdo->getObject('Details',$q);
     }
     function getDetails(){
         $details = $this->getMany('Details');
