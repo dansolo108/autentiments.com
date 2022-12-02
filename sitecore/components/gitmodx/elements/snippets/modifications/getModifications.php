a86502e46f54263d7cc6d7990dcfb4991b2f392b
skril <SZhukovWork@gmail.com>
2022-12-02 17:15:40 +0500

    fix bugs

--- sitecore/components/gitmodx/elements/snippets/modifications/getModifications.php
+++ sitecore/components/gitmodx/elements/snippets/modifications/getModifications.php
@@ -56,13 +56,13 @@ if(is_string($scriptProperties['groupby'])){
 // Include linked products
 $innerJoin = array();
 if (!empty($link) && !empty($master)) {
-    $leftJoin['Link'] = [
+    $innerJoin['Link'] = [
         'class' => 'msProductLink',
         'on' => 'msProduct.id = Link.slave AND Link.link = ' . $link,
     ];
     $where['Link.master'] = $master;
 } elseif (!empty($link) && !empty($slave)) {
-    $leftJoin['Link'] = [
+    $innerJoin['Link'] = [
         'class' => 'msProductLink',
         'on' => 'msProduct.id = Link.master AND Link.link = ' . $link,
     ];
