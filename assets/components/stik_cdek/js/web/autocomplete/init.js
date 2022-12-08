d13f8eeca864167de79e2b3ca04db82d22451640
skril <SZhukovWork@gmail.com>
2022-12-09 02:11:45 +0500

    fix bug

--- assets/components/stik_cdek/js/web/autocomplete/init.js
+++ assets/components/stik_cdek/js/web/autocomplete/init.js
@@ -1,39 +1,50 @@
 var cityAutocomplete;
 
 function initAutocomplete() {
+    var dadataToken = "da659a5364a0d433b8a5e2641e6d7f70390f8606";
     cityAutocomplete = new autoComplete({
         selector: 'input[name="city"]',
         minChars: 3,
         source: function (term, response) {
             term = term.toLowerCase();
-            $.getJSON('https://api.cdek.ru/city/getListByTerm/jsonp.php?callback=?', {
-                q: term,
-                name_startsWith: term,
-            }, function (data) {
-                var cityArr = data.geonames;
-                if (cityArr) {
-                    var suggestions = [];
-                    for (i = 0; i < cityArr.length; i++) {
-                        suggestions.push(cityArr[i]);
-                    }
-                    response(suggestions);
-                }
-            });
+            let body = {
+                query: term,
+                from_bound: { "value": "city" },
+                to_bound: { "value": "city" },
+            };
+            let countryElem = $('#country');
+            body.locations = [
+
+            ];
+            if(countryElem.attr('data-val')){
+                body.locations.push({
+                    "country":countryElem.attr('data-val'),
+                });
+            }
+            let options = {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                    "Authorization": "Token " + dadataToken
+                },
+                body: JSON.stringify(body)
+            }
+            fetch("https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address", options)
+                .then(response => response.text())
+                .then(result => {
+                    result = JSON.parse(result);
+                    console.log(result);
+                    result.suggestions.forEach(item=>{
+                        console.log(item.data.region_type_full,item.data.area_type_full,item.data.city_type_full,item.data.settlement_type_full,item.data.street_type_full);
+                    })
+                    response(result.suggestions);
+                })
+                .catch(error => console.log("error", error));
         },
         renderItem: function (item, search) {
-            if (item.postCodeArray) {
-                if (item.postCodeArray[1]) {
-                    var cdek_index = item.postCodeArray[1];
-                } else {
-                    var cdek_index = item.postCodeArray[0];
-                }
-            } else {
-                var cdek_index = 0;
-            }
-            
-            let cityArr = item.cityName.split(",");
-
-            return '<div class="autocomplete-suggestion" data-index="' + cdek_index + '" data-id="' + item.id + '" data-index="' + cdek_index + '" data-city="' + cityArr[0] + '" data-val="' + cityArr[0] + '">' + item.cityName + ' <small>[' + item.regionName + ']</small></div>';
+            return `<div class="autocomplete-suggestion" data-index="0" data-id="${item.data.kladr_id}" data-city="${item.data.city}" data-val="${(item.data.settlement || item.data.city)}">${(item.data.settlement_with_type || item.data.city_with_type)} <small>[${item.data.region_with_type}]</small></div>`;
         },
         onSelect: function (e, term, item) {
             $('[name=city]').val($(item).data('city'));
@@ -42,7 +53,27 @@ function initAutocomplete() {
                 $('[name=city]').change();
             }, 500);
             setTimeout(function () {
-                miniShop2.Order.add('cdek_id', $(item).data('id'));
+                let options = {
+                    method: "POST",
+                    mode: "cors",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Accept": "application/json",
+                        "Authorization": "Token " + dadataToken
+                    },
+                    body: JSON.stringify({
+                        query: $(item).data('id')
+                    })
+                }
+                fetch("https://suggestions.dadata.ru/suggestions/api/4_1/rs/findById/delivery", options)
+                    .then(response => response.text())
+                    .then(result => {
+                        result = JSON.parse(result);
+                        if(result.suggestions){
+                            miniShop2.Order.add('cdek_id', result.suggestions[0].data.cdek_id);
+                        }
+                    })
+                    .catch(error => console.log("error", error));
             }, 100);
         }
     });
