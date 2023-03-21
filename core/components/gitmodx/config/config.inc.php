09cd2940861c4b73ebab8e9e15207d75a9d92fc9
semyon <SZhukovWork@gmail.com>
2023-03-21 14:25:00 +0500

    fix bugs

--- core/components/gitmodx/config/config.inc.php
+++ core/components/gitmodx/config/config.inc.php
@@ -2,6 +2,5 @@
 $gitModxConfig = array(
         'stik_moySklad_api_key'=>'36f61839f3f41d0c14fc10f493a44139529be7ea',
         'stik_moySklad_url'=>'https://online.moysklad.ru/api/remap/1.2/',
-        "site_url"=>"http://localhost"
 );
 return $gitModxConfig;
