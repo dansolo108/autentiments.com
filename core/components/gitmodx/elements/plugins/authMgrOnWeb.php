f7974c921318245c5dc50e0dae801cfdb59ceb7a
semyon <SZhukovWork@gmail.com>
2023-03-21 14:42:53 +0500

    fix bugs

--- core/components/gitmodx/elements/plugins/authMgrOnWeb.php
+++ core/components/gitmodx/elements/plugins/authMgrOnWeb.php
@@ -1,8 +1,19 @@
 <?php
-if ($modx->event->name == 'OnWebPageInit') {
-    if (!$modx->user->hasSessionContext('web') && $modx->user->hasSessionContext('mgr') && $modx->user->isMember('Administrator')) {
-        $modx->user->addSessionContext('web');
-    } elseif ($modx->user->hasSessionContext('web') && !$modx->user->hasSessionContext('mgr') && $modx->user->isMember('Administrator')) {
-        $modx->user->removeSessionContext('web');
-    }
+switch ($modx->event->name) {
+    case 'OnWebPageInit':
+        if ($modx->context->key == 'mgr') {
+            return;
+        }
+
+        /** авторизовывает админа в админке (если он не авторизован на фронте, и является администратором) */
+        if (!$modx->user->hasSessionContext('mgr') && $modx->user->hasSessionContext($modx->context->key) && ($modx->user->get('sudo') || $modx->user->isMember('Administrator'))) {
+            $modx->user->addSessionContext('mgr');
+        }
+
+        /** авторизовывает на фронте любого юзера, который авторизован в админке */
+        if ($modx->user->hasSessionContext('mgr') && !$modx->user->hasSessionContext($modx->context->key)) {
+            $modx->user->addSessionContext($modx->context->key);
+        }
+
+        break;
 }
