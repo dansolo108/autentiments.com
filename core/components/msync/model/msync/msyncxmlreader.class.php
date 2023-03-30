39d4cd594dbf2bf55bfc530701182f01d20b2e27
semyon <SZhukovWork@gmail.com>
2023-03-30 05:51:02 +0500

    test

--- core/components/msync/model/msync/msyncxmlreader.class.php
+++ core/components/msync/model/msync/msyncxmlreader.class.php
@@ -39,11 +39,8 @@ class mSyncXmlReader
      */
     public function readXml($reader)
     {
-        $outerXml = $reader->readOuterXML();
         $this->msync->log(var_export($reader->readOuterXML(),1), 1);
-        $this->msync->log(var_export($outerXml,1), 1);
-        $this->msync->log(var_export($outerXml ? new SimpleXMLElement($outerXml) : null,1), 1);
-        return $outerXml ? new SimpleXMLElement($outerXml) : null;
+        return new SimpleXMLElement($reader->readOuterXML());
     }
 
     /**
