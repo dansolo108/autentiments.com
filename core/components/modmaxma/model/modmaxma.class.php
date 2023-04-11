ebed10a54ed65a16527cd9bf1b95d48e8167f4f9
semyon <SZhukovWork@gmail.com>
2023-04-11 16:39:48 +0500

    умножение заказов

--- core/components/modmaxma/model/modmaxma.class.php
+++ core/components/modmaxma/model/modmaxma.class.php
@@ -1,15 +1,15 @@
 <?php
-include_once MODX_CORE_PATH . '/model/modx/rest/modrest.class.php';
+include_once MODX_CORE_PATH."/model/modx/rest/modrest.class.php";
 class modMaxma
 {
     /** @var modX $modx */
     public $modx;
     /** @var pdoFetch $pdoTools */
     public $pdoTools;
-    public string $version = '1.0.1';
+    public string $version = "1.0.1";
 
     public array $config = [];
-    public string $settings_prefix = 'modmaxma_';
+    public string $settings_prefix = "modmaxma_";
 
     public modRest $modRest;
 
@@ -17,28 +17,28 @@ class modMaxma
 
     function __construct(modX &$modx, array $config = [])
     {
-        $this->modx = &$modx;
+        $this->modx =& $modx;
         /** @var modNamespace $namespace */
-        $namespace = $modx->getObject('modNamespace', 'modMaxma');
+        $namespace = $modx->getObject('modNamespace',"modMaxma");
         $corePath = $namespace->getCorePath();
         $assetsPath = $namespace->getAssetsPath();
-        $assetsUrl = str_replace(MODX_BASE_PATH, '', $assetsPath);
-        if ($assetsUrl[0] !== '/') {
-            $assetsUrl = '/' . $assetsUrl;
+        $assetsUrl = str_replace(MODX_BASE_PATH,"",$assetsPath);
+        if($assetsUrl[0] !== "/"){
+            $assetsUrl = "/".$assetsUrl;
         }
         //дефолтные значения заменяем значениями из настроек
         $this->config = $this->getOptions([
             'core_path' => $corePath,
             'model_path' => $corePath . 'model/',
             'processors_path' => $corePath . 'processors/',
-            'polling_rate' => 5,
+            "polling_rate"=>5,
             //front
-            //'frontend_js' =>$assetsUrl . 'js/web/default.js',
-            //'frontend_css' =>$assetsUrl . 'css/default.css',
+            'frontend_js' =>$assetsUrl . 'js/web/default.js',
+            'frontend_css' =>$assetsUrl . 'css/default.css',
             'action_url' => $assetsUrl . 'action.php',
             'assets_url' => $assetsUrl,
-            //'css_url' => $assetsUrl . 'css/',
-            //'js_url' => $assetsUrl . 'js/',
+            'css_url' => $assetsUrl . 'css/',
+            'js_url' => $assetsUrl . 'js/',
             //api
             'apiKey' => $this->modx->getOption('stik_maxma_api_key'),
             'serverAddress' => $this->modx->getOption('stik_maxma_url'), // api-test.cloudloyalty.ru
@@ -61,33 +61,29 @@ class modMaxma
             'Content-type' => 'application/json', // Сообщаем сервису, что хотим получить ответ в json формате
             'X-Processing-Key' => $this->config['apiKey']
         ]);
-        $this->ms2 = &$modx->getService('minishop2');
-        if (empty($this->ms2)) {
-            $this->modx->log(MODX_LOG_LEVEL_ERROR, 'could not load minishop');
-            throw new ErrorException('could not load minishop');
+        $this->ms2 = &$modx->getService("minishop2");
+        if(empty($this->ms2)){
+            $this->modx->log(MODX_LOG_LEVEL_ERROR,"could not load minishop");
+            throw new ErrorException("could not load minishop");
         }
         $this->ms2->initialize();
         $this->loadFrontend();
     }
-    function getOption($key, $options = null, $default = null, $skipEmpty = false)
-    {
-        return $this->modx->getOption($this->settings_prefix . $key, $options, $default, $skipEmpty);
+    function getOption($key, $options = null, $default = null, $skipEmpty = false){
+        return $this->modx->getOption($this->settings_prefix.$key, $options, $default, $skipEmpty);
     }
 
-    function getOptions($defaultOptions, $prefix = '')
-    {
-        foreach ($defaultOptions as $key => &$option) {
-            $option = $this->getOption($prefix . $key, null, $option);
+    function getOptions($defaultOptions,$prefix = ""){
+        foreach ($defaultOptions as $key => &$option){
+            $option = $this->getOption($prefix.$key,null,$option);
         }
         return $defaultOptions;
     }
 
-    function loadFrontend()
-    {
+    function loadFrontend(){
         $config = $this->pdoTools->makePlaceholders($this->config);
-
         // Register JS
-        $js = trim($this->config['frontend_js']);
+        $js = trim($this->config["frontend_js"]);
         if (!empty($js) && preg_match('/\.js/i', $js)) {
             if (preg_match('/\.js$/i', $js)) {
                 $js .= '?v=' . substr(md5($this->version), 0, 10);
@@ -95,19 +91,17 @@ class modMaxma
             $this->modx->regClientScript(str_replace($config['pl'], $config['vl'], $js));
             // готовим js конфиг на фронт
             $js_settings = [
-                'action_url' => $this->config['action_url'],
+                'action_url'=>$this->config["action_url"],
             ];
             $data = json_encode($js_settings);
             $this->modx->regClientHTMLBlock(
-                "<script>
+            "<script>
                     document.addEventListener('miniShop2Initialize',e=>{
                         window.miniShop2.modMaxma = new modMaxma({$data});
                     })
-                </script>"
-            );
+                </script>");
         }
-
-        $css = trim($this->config['frontend_css']);
+        $css = trim($this->config["frontend_css"]);
         if (!empty($js) && preg_match('/\.css/i', $css)) {
             if (preg_match('/\.css$/i', $css)) {
                 $css .= '?v=' . substr(md5($this->version), 0, 10);
@@ -116,40 +110,41 @@ class modMaxma
         }
     }
     // Создание/изменение заказа
-    public function setOrder(int $order_id)
-    {
+    public function setOrder(int $order_id) {
         $order = $this->modx->getObject('msOrder', $order_id);
         $products = $order->getMany('Products');
         $queryArr = [
-            'orderId' => $order_id,
-            'calculationQuery' => [
-                'client' => [
-                    'externalId' => (string)$order->get('user_id')
+            "orderId"=>$order_id,
+            "calculationQuery"=>[
+                "client"=>[
+                    "externalId"=>(string)$order->get("user_id")
                 ],
-                'shop' => [
-                    'code' => $this->config['shopCode'],
-                    'name' => $this->config['shopName'],
+                "shop"=>[
+                    "code"=>$this->config["shopCode"],
+                    "name"=>$this->config["shopName"],
                 ],
-                'rows' => [],
+                "rows"=>[
+
+                ],
             ],
         ];
-        foreach ($products as $orderProduct) {
+        foreach ($products as $orderProduct){
             $product = $orderProduct->getOne('Product');
-            $modification = $this->modx->getObject('Modification', $orderProduct->get('modification_id'));
-            $price = $modification->get('price');
-            $orderPrice = $orderProduct->get('price');
+            $modification = $this->modx->getObject("Modification",$orderProduct->get("modification_id"));
+            $price = $modification->get("price");
+            $orderPrice = $orderProduct->get("price");
             $productArr = [
-                'id' => $orderProduct->get('id'),
-                'product' => [
-                    'sku' => $modification->get('code') ?: $product->get('article'),
-                    'title' => $product->get('pagetitle'),
-                    'blackPrice' => $price,
+                "id"=>$orderProduct->get("id"),
+                "product"=>[
+                    "sku"=>$modification->get("code")?:$product->get("article"),
+                    "title"=>$product->get("pagetitle"),
+                    "blackPrice"=>$price,
                 ],
-                'qty' => $orderProduct->get('count'),
+                "qty"=>$orderProduct->get("count"),
             ];
-            if ($price > $orderPrice) {
-                $productArr['product']['blackPrice'] = $price;
-                $productArr['product']['redPrice'] = $orderPrice;
+            if($price > $orderPrice){
+                $productArr["product"]["blackPrice"] = $price;
+                $productArr["product"]["redPrice"] = $orderPrice;
             }
             $queryArr['rows'][] = $productArr;
         }
@@ -164,9 +159,8 @@ class modMaxma
         }
     }
     // Подтверждение оплаты заказа
-    public function confirmOrder(string $order_id)
-    {
-        $response = $this->modRest->post('confirm-order', ['orderId' => $order_id]);
+    public function confirmOrder(string $order_id) {
+        $response = $this->modRest->post('confirm-order', ["orderId"=>$order_id]);
         $data = $response->process();
 
         if (isset($data['errorCode'])) {
@@ -178,9 +172,8 @@ class modMaxma
     }
 
     // Отмена заказа и возврат зарезервированных бонусов
-    public function cancelOrder(string $order_id)
-    {
-        $response = $this->modRest->post('cancel-order', ['orderId' => $order_id]);
+    public function cancelOrder(string $order_id) {
+        $response = $this->modRest->post('cancel-order', ["orderId"=>$order_id]);
         $data = $response->process();
 
         if (isset($data['errorCode'])) {
@@ -197,23 +190,22 @@ class modMaxma
      * @param string $promocode
      * @return array|false
      */
-    public function calculatePurchase($cart, $user_id = null, $bonuses = 0, string $promocode = '')
-    {
+    public function calculatePurchase($cart ,$user_id = null, $bonuses = 0, string $promocode = ''){
         if (count($cart) == 0) {
             return false;
         }
         $params = [
             'calculationQuery' => [
                 'shop' => [
-                    'code' => $this->config['shopCode'],
-                    'name' => $this->config['shopName'],
+                    "code" => $this->config['shopCode'],
+                    "name" => $this->config['shopName'],
                 ],
-                'applyBonuses' => $bonuses,
-                'rows' => [],
+                "applyBonuses"=> $bonuses,
+                "rows" => [],
             ]
         ];
-        if ($user_id) {
-            $params['calculationQuery']['client']['externalId'] = (string)$user_id;
+        if($user_id){
+            $params["calculationQuery"]["client"]["externalId"] = (string)$user_id;
         }
         if ($promocode) {
             $params['calculationQuery']['promocode'] = $promocode;
@@ -226,12 +218,12 @@ class modMaxma
             if (empty($entry['price']) || empty($entry['count']))
                 continue;
             $params['calculationQuery']['rows'][] = [
-                'product' => [
-                    'sku' => (string)$modification->get('code') ?: $product->get('article'),
-                    'title' => $product->get('pagetitle'),
-                    'blackPrice' => $modification->get('price'),
+                "product" => [
+                    "sku" => (string)$modification->get('code') ?: $product->get('article'),
+                    "title" => $product->get('pagetitle'),
+                    "blackPrice" => $modification->get('price'),
                 ],
-                'qty' => (int)$entry['count'],
+                "qty" => (int)$entry['count'],
             ];
         }
         if (count($params['calculationQuery']['rows']) == 0) {
@@ -248,20 +240,19 @@ class modMaxma
             return $data;
         }
     }
-    public function calculateCurrentOrder($bonuses = null, $promocode = null)
-    {
+    public function calculateCurrentOrder($bonuses = null,$promocode=null){
         $order = $this->ms2->order->get();
         $user_id = null;
-        if ($user =  $this->modx->getAuthenticatedUser('web')) {
+        if($user =  $this->modx->getAuthenticatedUser("web")){
             $user_id = $user->get('id');
         }
-        if ($promocode == null) {
-            $promocode = $order['promocode'] ?: '';
+        if($promocode == null){
+            $promocode = $order["promocode"]?:"";
         }
-        if ($bonuses  == null) {
-            $bonuses = $order['bonuses'] ?: 0;
+        if($bonuses  == null){
+            $bonuses = $order["bonuses"]?:0;
         }
-        return $this->calculatePurchase($this->ms2->cart->config['cart'], $user_id, $bonuses, $promocode);
+        return $this->calculatePurchase($this->ms2->cart->config["cart"],$user_id,$bonuses,$promocode);
     }
 
 
@@ -279,4 +270,6 @@ class modMaxma
             return $data;
         }
     }
+
+
 }
