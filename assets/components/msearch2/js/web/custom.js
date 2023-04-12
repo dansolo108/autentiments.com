dd5eb11d4711048f726229eb579a3bd583c7eb11
DanSoloRu <me@dansolo.ru>
2023-04-12 18:28:46 +0300

    на пути к фильтрам

--- assets/components/msearch2/js/web/custom.js
+++ assets/components/msearch2/js/web/custom.js
@@ -637,7 +637,8 @@ var mSearch2 = {
         for (var i in params) {
             if (params.hasOwnProperty(i) && this.options.reset_skip.indexOf(i) === -1) {
                 this.btn_reset.removeClass('hidden');
-                this.btn_reset.innerHTML += ` (${params.length})`;
+                console.log(params)
+                // this.btn_reset.innerHTML += ` (${params.length})`;
                 break;
             }
         }
