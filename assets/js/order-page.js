d84ad87750438498225eecfe829e0254eea4b373
DanSoloRu <me@dansolo.ru>
2023-04-13 16:36:40 +0300

    на пути к корзине мечты

--- assets/js/order-page.js
+++ assets/js/order-page.js
@@ -87,16 +87,18 @@ document.addEventListener("radio-active",e=>{
     // method?.querySelector(`.auten-delivery-method__pickup`)?.classList.add("outline");
     method.querySelector(`.auten-delivery-method__pickup`).textContent = "Изменить пункт";
 })
-document.addEventListener("radio-inactive",e => {
-    e.target.closest(`.auten-pickup-point`)?.classList.remove(`active`);
-    e.target.closest(`.auten-pickup-point`)?.nextElementSibling?.remove();
+document.addEventListener("radio-inactive",e=> {
+    let target = e.target.closest(`.auten-pickup-point`),
+    btn = target.nextElementSibling;
+    target?.classList.remove(`active`);
+    target?.parentNode.removeChild(btn);
 })
-document.addEventListener("change",e=>{
+document.addEventListener("change",e => {
     let method = e.target.closest(`.auten-delivery-method`);
     if(!method || !e.target.closest(`input[name=delivery]`))
         return;
     // method.querySelector(`.auten-delivery-method__pickup`)?.classList.remove("outline");
-    method.querySelector(`.auten-delivery-method__pickup`).textContent = "Выбрать пункт";
+    method.querySelector(`.auten-delivery-method__pickup`)?.textContent = "Выбрать пункт";
 })
 
 document.addEventListener("modal-open",e=>{
