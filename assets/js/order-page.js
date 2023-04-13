e268fe4ed6e88f062c209e7fcac6d6fe190d3cf8
DanSoloRu <me@dansolo.ru>
2023-04-13 16:40:11 +0300

    на пути к корзине мечты

--- assets/js/order-page.js
+++ assets/js/order-page.js
@@ -91,14 +91,14 @@ document.addEventListener("radio-inactive",e=> {
     let target = e.target.closest(`.auten-pickup-point`),
     btn = target.nextElementSibling;
     target?.classList.remove(`active`);
-    target?.parentNode.removeChild(btn);
+    target.parentNode.removeChild(btn);
 })
 document.addEventListener("change",e => {
     let method = e.target.closest(`.auten-delivery-method`);
     if(!method || !e.target.closest(`input[name=delivery]`))
         return;
     // method.querySelector(`.auten-delivery-method__pickup`)?.classList.remove("outline");
-    method.querySelector(`.auten-delivery-method__pickup`)?.textContent = "Выбрать пункт";
+    method.querySelector(`.auten-delivery-method__pickup`).textContent = "Выбрать пункт";
 })
 
 document.addEventListener("modal-open",e=>{
